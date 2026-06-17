8925dadbd55a4cdf921636d8059a479a0957fc74
user <kyryle0110@gmail.com>
2026-06-17 14:45:58 +0300

    again

---
+++ src/js/1-timer.js
@@ -0,0 +1,21 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+const btn = document.querySelector(".btn")
+let userSelectedDate = []
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+        if (selectedDates < new Date) {
+            window.alert("Please choose a date in the future")
+            btn.disabled = true;
+        } else
+            btn.disabled = false;
+            userSelectedDate = selectedDates
+  },
+};
+
+flatpickr("#datetime-picker", options);
